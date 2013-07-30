a11b96a0a13273cb25c4b6d7d018ae23c6225c11
carlos <carlos@flemming.cnb.csic.es>
2013-07-30 11:26:59 +0200

    Conseguido el RG plugin. Ahora a intentar el cubo RGB

---
+++ PluginTest/PluginTest/src/Plugin_RedGreenScale.java
@@ -0,0 +1,25 @@
+import ij.ImagePlus;
+import ij.plugin.PlugIn;
+import ij.process.ColorProcessor;
+import ij.process.ImageProcessor;
+
+
+public class Plugin_RedGreenScale implements PlugIn 
+{ 
+	public void run(String arg) 
+	{ 
+		int width = 256; 
+		int height = 256; 
+		ImageProcessor ip = new ColorProcessor(width, height); 
+		for(int y = 0; y < height; y++)
+		{
+			for (int x = 0; x < width; x++)
+			{
+				int[] col = {x, y, 0};
+				ip.putPixel(x,y,col);
+			}
+		}
+		ImagePlus imp = new ImagePlus("Result image", ip); 
+		imp.show();
+	}
+}
