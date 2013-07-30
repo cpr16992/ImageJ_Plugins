f26f2cba2b63514714eac76049246e2755111fc3
carlos <carlos@flemming.cnb.csic.es>
2013-07-30 11:18:55 +0200

    Conseguido hacer funcionar RedScale

--- PluginTest/PluginTest/src/Plugin_RedScale.java
+++ PluginTest/PluginTest/src/Plugin_RedScale.java
@@ -3,7 +3,7 @@ import java.awt.Color;
 import ij.ImagePlus;
 import ij.gui.Roi;
 import ij.plugin.PlugIn;
-import ij.process.ByteProcessor;
+import ij.process.ColorProcessor;
 import ij.process.ImageProcessor;
 
 
@@ -13,12 +13,12 @@ public class Plugin_RedScale implements PlugIn
 	{ 
 		int width = 256; 
 		int height = 256; 
-		ImageProcessor ip = new ByteProcessor(width, height); 
+		ImageProcessor ip = new ColorProcessor(width, height); 
 		for(int y = 0; y < height; y++)
 		{
 			Color arg0 = new Color(y,0,0);
 			Roi line = new Roi(1,y,width,1);
-			line.setColor(arg0);
+			ip.setColor(arg0);
 			ip.fill(line);			
 		}
 		ImagePlus imp = new ImagePlus("Result image", ip); 
